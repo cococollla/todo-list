e06405bedafecb53b0cda58be628c4f86ee9696f
nsbel <nsbelyaev@outlook.com>
2023-12-29 11:29:15 +0300

    Fix error in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Todo from "./components/Todo";
+import Todo from "./components/ToDo";
 import TodoForm from "./components/TodoForm";
 import "./index.css";
 
